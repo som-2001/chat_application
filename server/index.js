c4a73a6facc2f4a9d0acbf8441903592c0205ab1
som-2001 <100964164+som-2001@users.noreply.github.com>
2023-12-30 15:32:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const server = require('http').createServer(app);
+const server = require('https').createServer(app);
 const io = require('socket.io')(server, { 
   cors: { 
     origin: 'https://chat-application-ocy9.vercel.app',  // Update with your React app's domain
