d918f784a50705c3e84b3d3bb2fff7a81ab4146a
som-2001 <100964164+som-2001@users.noreply.github.com>
2023-12-30 19:30:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,20 +5,19 @@ const app = express();
 const server = require('https').createServer(app);
 const io = require('socket.io')(server, { 
   cors: { 
-    origin: 'https://chat-application-ocy9.vercel.app/,  // Update with your React app's domain
+    origin: 'https://chat-application-ocy9.vercel.app',  // Update with your React app's domain
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   } 
 });
 const port = process.env.PORT;
 
-app.use(
-  cors({
-    origin: ["https://chat-application-ocy9.vercel.app"],
-    methods: ["GET", "POST", "PUT"],
-    credentials: true,
-  })
-); 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://chat-application-ocy9.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
